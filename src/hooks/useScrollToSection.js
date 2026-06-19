26d35f48921df5622c654e973958db46f310a644
darkboy0p <231809516+darkboy0p@users.noreply.github.com>
2026-06-19 11:58:16 +0600

    Create hooks/useScrollToSection.js

---
+++ src/hooks/useScrollToSection.js
@@ -0,0 +1,15 @@
+import { useCallback } from 'react';
+
+export const useScrollToSection = () => {
+  const scrollToSection = useCallback((sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }, []);
+
+  return scrollToSection;
+};
